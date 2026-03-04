f95a46857c06e80fddbc8244f7dd789ecdf5c71e
rabinarayanpadhy07 <132149572+rabinarayanpadhy07@users.noreply.github.com>
2026-03-04 18:26:32 +0530

    Added img uload to aws

--- src/components/atoms/Editor/Edtior.jsx
+++ src/components/atoms/Editor/Edtior.jsx
@@ -1,6 +1,6 @@
 import 'quill/dist/quill.snow.css'; // ES6
 
-import { ImageIcon } from 'lucide-react';
+import { ImageIcon, XIcon } from 'lucide-react';
 import Quill from 'quill';
 import { useEffect, useRef, useState } from 'react';
 import { MdSend } from 'react-icons/md';
@@ -19,9 +19,12 @@ export const Editor = ({
 
     const [isToolbarVisible, setIsToolbarVisible] = useState(false);
 
+    const [image, setImage] = useState(null);
+
     const containerRef = useRef(); // reqd to initialize the editor
     const defaultValueRef = useRef();
     const quillRef = useRef();
+    const imageInputRef = useRef(null);
 
     function toggleToolbar() {
         setIsToolbarVisible(!isToolbarVisible);
@@ -87,6 +90,29 @@ export const Editor = ({
                 className='flex flex-col border border-slate-300 rounded-md overflow-hidden focus-within:shadow-sm focus-within:border-slate-400 bg-white '
             >
                 <div className='h-full ql-custom' ref={containerRef} />
+                {
+                    image && (
+                        <div
+                            className='p-2'
+                        >
+                            <div className='relative size-[60px] flex items-center justify-center group/image'>
+                                <button
+                                    className='hidden group-hover/image:flex rounded-full bg-black/70 hover:bg-black absolute -top-2.5 -right-2.5 text-white size-6 z-[5] border-2 border-white items-center justify-center'
+                                    onClick={() => {
+                                        setImage(null);
+                                        imageInputRef.current.value = '';
+                                    }}
+                                >
+                                    <XIcon className='size-4' />
+                                </button>
+                                <img 
+                                    src={URL.createObjectURL(image)}
+                                    className='rounded-xl overflow-hidden border object-cover'
+                                />
+                            </div>
+                        </div>
+                    )
+                }
 
                 <div className='flex px-2 pb-2 z-[5]'>
                     <Hint label={!isToolbarVisible ? 'Show toolbar' : 'Hide toolbar'} side='bottom' align='center'>
@@ -105,11 +131,18 @@ export const Editor = ({
                             size="iconSm"
                             variant="ghost"
                             disabled={false}
-                            onClick={() => {}}
+                            onClick={() => { imageInputRef.current.click(); }}
                         >
                             <ImageIcon className='size-4' />
                         </Button>
                     </Hint>
+
+                    <input 
+                        type="file"
+                        className='hidden'
+                        ref={imageInputRef}
+                        onChange={(e) => setImage(e.target.files[0])}
+                    />
 
                     <Hint label="Send Message">
                         <Button
@@ -117,8 +150,10 @@ export const Editor = ({
                             className="ml-auto bg-[#007a6a] hover:bg-[#007a6a]/80 text-white"
                             onClick={() => {
                                 const messageContent = JSON.stringify(quillRef.current?.getContents());
-                                onSubmit({ body: messageContent });
+                                onSubmit({ body: messageContent, image });
                                 quillRef.current?.setText('');
+                                setImage(null);
+                                imageInputRef.current.value = '';
                             }}
                             disabled={false}
                         >
