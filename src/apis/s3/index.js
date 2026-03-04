7913fc3954f8bd407391d0d84a193005707fba40
rabinarayanpadhy07 <132149572+rabinarayanpadhy07@users.noreply.github.com>
2026-03-04 18:26:32 +0530

    Added img uload to aws

--- src/apis/s3/index.js
+++ src/apis/s3/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import axiosConfig from '../../config/axiosConfig';
 
 export const uploadImageToAWSpresignedUrl = async ({ url, file }) => {
     try {
@@ -14,3 +15,16 @@ export const uploadImageToAWSpresignedUrl = async ({ url, file }) => {
         console.log('Error in uploading image to s3', error);
     }
 };
+
+export const getPreginedUrl = async ({ token }) => {
+    try {
+        const response = await axiosConfig.get('/messages/pre-signed-url', {
+            headers: {
+                'x-access-token': token
+            }
+        });
+        return response?.data?.data;
+    } catch (error) {
+        console.log('Error in getting presigned url', error);
+    }
+}
